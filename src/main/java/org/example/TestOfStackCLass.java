ca13157760d8894ebfa16803e4ebb7b3ae51f431
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-03-27 10:44:15 -0400

    These method comes from the class Stack

--- src/main/java/org/example/TestOfStackCLass.java
+++ src/main/java/org/example/TestOfStackCLass.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Stack;
+
 public class TestOfStackCLass {
     public static void main(String[] args) {
+        Stack s = new Stack();
 
+        s.add("vanier");
+        s.add('c');
+        s.pop();
+
+        s.push(1);
+
+        System.out.println(s);
     }
 }
